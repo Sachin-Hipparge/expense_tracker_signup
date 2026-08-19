15dfba588f986dc9e84cd7a7dfdd4b442893aa67
Sachin-Hipparge <sachinhipparge064@gmail.com>
2026-08-19 22:30:01 +0530

    created user login page and connected with it database

--- app.js
+++ app.js
@@ -50,6 +50,43 @@ app.post("/user/signup", (req, res) => {
         );
     });
 });
+app.post("/user/login", (req, res) => {
+
+    const { email, password } = req.body;
+
+    const sql = "SELECT * FROM users WHERE email = ?";
+
+    db.execute(sql, [email], (err, results) => {
+
+        if (err) {
+            console.log(err);
+            return res.status(500).json({
+                message: "Something went wrong"
+            });
+        }
+
+        // Email not found
+        if (results.length === 0) {
+            return res.status(401).json({
+                message: "Invalid email"
+            });
+        }
+
+        const user = results[0];
+
+        // Password doesn't match
+        if (user.password !== password) {
+            return res.status(401).json({
+                message: "Incorrect password"
+            });
+        }
+
+        // Email and password match
+        res.status(200).json({
+            message: "Login successful"
+        });
+    });
+});
 
 app.listen(3000, () => {
     console.log("Server running on port 3000");
