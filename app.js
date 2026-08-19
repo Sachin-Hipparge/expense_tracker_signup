7e1f84897d210bbee6d6eaad8ea2e734e1c8da53
Sachin-Hipparge <sachinhipparge064@gmail.com>
2026-08-19 17:21:16 +0530

    connected frontend with backend the data entered by the user now it will be stored in the database

---
+++ app.js
@@ -0,0 +1,56 @@
+const express = require("express");
+const cors = require("cors");
+const db = require("./utils/database");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.post("/user/signup", (req, res) => {
+
+    const { name, email, password } = req.body;
+
+    const sql = "SELECT * FROM users WHERE email = ?";
+
+    db.execute(sql, [email], (err, results) => {
+
+        if (err) {
+            console.log(err);
+            return res.status(500).json({
+                message: "Something went wrong"
+            });
+        }
+
+        if (results.length > 0) {
+            return res.status(409).json({
+                message: "User already exists"
+            });
+        }
+
+        const insertSql =
+            "INSERT INTO users (name, email, password) VALUES (?, ?, ?)";
+
+        db.execute(
+            insertSql,
+            [name, email, password],
+            (err, result) => {
+
+                if (err) {
+                    console.log(err);
+                    return res.status(500).json({
+                        message: "Something went wrong"
+                    });
+                }
+
+                res.status(201).json({
+                    message: "User created successfully"
+                });
+            }
+        );
+    });
+});
+
+app.listen(3000, () => {
+    console.log("Server running on port 3000");
+});
